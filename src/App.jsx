da792ebded22b77f0419f7200e3a626ffbbb15dd
Ikramov_21 <oybek.ikramov.official@gmail.com>
2024-03-31 16:23:31 +0500

    added vercel json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
+import {Route, Routes} from "react-router-dom";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
